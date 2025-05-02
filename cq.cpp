0d7f03956390e7e1c91f2c66d6a95dbbb647300e
YasharthSr7 <145427615+YasharthSr7@users.noreply.github.com>
2025-05-02 07:06:59 +0530

    Update cq.cpp

--- cq.cpp
+++ cq.cpp
@@ -1,92 +1,74 @@
-// Circular array 
-
-
 #include <bits/stdc++.h>
-
 using namespace std;
 
-class Circular_q{
-    public:    
-    int arr[30] , n;
-    int front = -1 ,rear = -1;
-    Circular_q(){
-        cout<<"Enter the size of array: ";
-        cin>>n;
+class Circular_q {
+public:
+    int arr[30], n;
+    int front = -1, rear = -1;
+
+    Circular_q() {
+        cout << "Enter the size of Queue: ";
+        cin >> n;
     }
-    void disp(){
-        int i  = front;
-        if(rear == -1){
-            cout<<"Queue is empty\n";
+
+    void disp() {
+        if (front == -1) {
+            cout << "Queue is empty\n";
+            return;
         }
-        else{
-            cout<<"Queue: ";
-        while (i != rear+1)
-        {
-            cout<<arr[i]<<" ";
-            i = (i+1)%n;
+        cout << "Queue: ";
+        int i = front;
+        while (true) {
+            cout << arr[i] << " ";
+            if (i == rear) break;
+            i = (i + 1) % n;
         }
-        }
+        cout << endl;
     }
-    void enque(){
-        cout<<"Enter the number to be enqued: ";
-        int a; cin>>a;
-        if(front == -1 and rear == -1){
+
+    void enque() {
+        cout << "Enter the number to be enqueued: ";
+        int a; cin >> a;
+        if (front == -1 && rear == -1) {
             arr[0] = a;
-            front = 0 ;rear = 0;
+            front = rear = 0;
         }
-        else if((rear + 1) % n == 0){
-            cout<<"Overflow\n";
+        else if ((rear + 1) % n == front) {
+            cout << "Overflow\n";
         }
-        else{
-            rear = rear + 1 %n;
+        else {
+            rear = (rear + 1) % n;
             arr[rear] = a;
         }
     }
-    void deque(){
-        if(front == -1){
-            cout<<"Underflow\n";
+
+    void deque() {
+        if (front == -1) {
+            cout << "Underflow\n";
         }
-        else if(front == rear){
-            front = -1;
-            rear = -1;
+        else if (front == rear) {
+            front = rear = -1;
         }
-        else{
-            arr[front] = 0;
+        else {
             front = (front + 1) % n;
         }
     }
 };
 
-
-
-int main(){
+int main() {
     Circular_q n;
-    int ch;
-    while (ch != 4)
-    {
-         cout<<"\n"
-                <<"1. Display Queue\n"
-                <<"2. Enque Queue\n"
-                <<"3. Deque Queue\n"
-                <<"4. Exit\n"
-                <<"Select operation: \n";
-                cin>>ch;
-        switch (ch)
-        {
-        case 1:
-            n.disp();
-            break;
-        case 2:
-            n.enque();
-            break;
-        case 3:
-            n.deque();
-            break;
-        default:
-            cout<<"Select Correct Option\n";
-            break;
+    int ch = 0;
+    while (ch != 4) {
+        cout << "\n1. Display Queue\n2. Enqueue\n3. Dequeue\n4. Exit\nSelect operation: ";
+        cin >> ch;
+        switch (ch) {
+            case 1: n.disp(); break;
+            case 2: n.enque(); break;
+            case 3: n.deque(); break;
+            case 4: cout << "Exiting...\n"; break;
+            default: cout << "Select Correct Option\n"; break;
         }
     }
-    
     return 0;
 }
+
